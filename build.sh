7776af3dadbc7cae9f04c5088a792293093cf4a8
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-04-08 12:44:16 +0300

    valgrind-debuggaus siirretty rinnakkaisohjelmaan

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "debug" ]; then
    cmake -D CMAKE_C_COMPILER=gcc-10 -D CMAKE_CXX_COMPILER=g++-10 -DCMAKE_BUILD_TYPE=Debug -S src -B build
    cmake --build build
-   valgrind build/smart_ptr
+   valgrind build/rinnakkaisuus
    exit;
 fi
 
